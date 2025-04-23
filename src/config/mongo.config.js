15e529d1396d52ed45d577a5d8d1bb266f16a5be
Yalef Amaya Morales <yalefamayamorales@MacBook-Air-de-Yalef.local>
2025-04-22 21:05:20 -0500

    [+] Mongo config

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function dbConection() {
     try {
-        await mongoose.connect( 'mongodb://localhost:27017/db-onest', {} );
+        await mongoose.connect( 'mongodb+srv://yaam2112:<db_password>@onest.ppdonsm.mongodb.net/?retryWrites=true&w=majority&appName=Onest', {} );
         console.log( 'Base de datos inicializada correctamente');
     } catch (error) {
         console.error(error);
